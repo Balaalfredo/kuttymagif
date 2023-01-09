b716b26df6f2c6a2918e4bbc605886d2c65a3f72
Balaalfredo <balas6664@gmail.com>
2023-01-09 20:23:32 +0530

    'second'

--- java.js
+++ java.js
@@ -1,3 +1,22 @@
 
+const imgg = document.querySelectorAll('.imgg');
 
+imgg.forEach((a) => {
+
+    a.addEventListener('click', () => {
+
+        // if (a.classList.contains('selectimg')) {
+        //     a.classList.remove('selectimg');
+        // }
+        // else { a.classList.add('selectimg'); }
+
+        for (i of imgg) {
+            i.classList.remove('selectimg');
+
+        }
+        a.classList.add('selectimg');
+
+    })
+
+})
 
